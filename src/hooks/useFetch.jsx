59b59d55499d101af7e6bc3d31e17073a4c69ed7
ponochovny <ponochovny@gmail.com>
2022-06-18 22:46:01 +0300

    removed axios and used fetch. formatted

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,15 +1,10 @@
-import axios from 'axios'
-
 const useFetch = () => {
 	const MAIN_URL = 'https://fcc-weather-api.glitch.me/api/current'
 
 	const fetchData = async (coords) => {
 		try {
-			const res = await axios.get(
-				`${MAIN_URL}?lat=${coords.lat}&lon=${coords.lon}`
-			)
-			console.log(res.data)
-			return res.data
+			const res = await fetch(`${MAIN_URL}?lat=${coords.lat}&lon=${coords.lon}`)
+			return res.json()
 		} catch (error) {
 			console.log(error)
 		}
